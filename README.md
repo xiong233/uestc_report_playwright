2f1bba6299baf3847c2842f65296a7422e9c2cab
xiong233 <2504588249@qq.com>
2022-04-24 16:31:13 +0800

    init

---
+++ README.md
@@ -0,0 +1,60 @@
+<h1 align="center">UESTC 自动健康打卡</h1>
+
+电子科技大学研究生每日自动健康打卡。
+
+## 前言
+
+1、本项目将在每天早上7点半左右通过信息门户账号登陆[健康打卡系统](https://eportal.uestc.edu.cn/jkdkapp/sys/lwReportEpidemicStu/*default/index.do) 进行健康打卡。
+
+2、本项目打卡所提交的内容（比如是否留校，是否隔离等）取决于你昨天的打卡内容，如果有变化请及时手动更新自己的状态。
+
+3、本项目创立的初衷在于技术交流、相互学习，而并非为学生提供打卡服务。因此，该项目不对提供的服务质量作出保障。
+
+4、如果您决定使用该项目所提供的服务，那么其带来的一切后果，均将由您个人承担，与原作者、贡献者无关。
+
+## Action使用教程
+
+1、点击右上角Fork本项目。
+
+2、进入到Fork后的项目，点击Action，开启workflow。
+
+3、依次点击Setting-Secrets-Actions。
+
+点击右上角New repository secret；在name下输入username，在value下输入你的学号；保存。
+
+点击右上角New repository secret；在name下输入password，在value下输入你的密码；保存。
+
+4、至此部署完成，每天早上将会自动打卡，如果你没有禁用 GitHub 的邮件通知，打卡失败将收到邮件提醒。
+
+5、如果你想关闭自动打卡，点击Action-DailyReport，再点击右上角三个小圆点，在下拉栏里点击Disable workflow。
+
+
+## 本地使用教程
+要求Python版本 > 3.7，需要自己设置每日定时任务
+
+下载本项目
+
+`git clone https://github.com/xiong233/uestc_report_playwright`
+
+`cd uestc_report_playwright`
+
+安装环境
+
+`pip install -r requirements.txt`
+
+`playwright install webkit
+`
+
+运行打卡项目
+
+`python main.py 201***** mypassword`
+
+## 其他
+
+1、Github服务器有时候会打不开学校的打卡网站(早上居然进不去电脑端的打卡网站)，因此本项目将会在第一次打卡失败后再尝试一次，但只要第一次打卡失败你就会收到邮件提醒。
+
+2、本项目的滑动验证识别部分用的[onion-rain/uestc_health_report](https://github.com/onion-rain/uestc_health_report) 的代码。
+
+3、Selenium会被判定为爬虫，之前先用Pyppeter做了一个[打卡项目](https://github.com/xiong233/uestc_report_public) 
+但是存在一些让人不舒服的地方（a,各种坑还搜不到解决方案; b,无头模式在本项目中也会判定爬虫; c,action每次打卡都要传输chrome压缩包）。
+所以本项目基于[Playwright](https://github.com/microsoft/playwright-python) 实现，语法跟Pyppeter差不多
---
+++ main.py
@@ -0,0 +1,129 @@
+from time import time
+import base64
+from playwright.sync_api import sync_playwright
+from slide import SlideCrack
+import sys
+
+username = sys.argv[1]
+password = sys.argv[2]
+
+check_button = '#app > div > div.mint-tabbar.mt-bg-lv3.mt-bColor-grey-lv6.is-fixed > a.mint-tab-item.is-selected > div.mint-tab-item-icon > i'
+add_button = '#app > div > div.mint-layout-container.pjcse52gj > div.mint-fixed-button.mt-color-white.sjarvhx43.mint-fixed-button--bottom-right.mint-fixed-button--primary.mt-bg-primary'
+save_button = "#app > div > div > div.mint-layout-container.OPjctwlgzsl > button"
+confirm_button = "body > div.mint-msgbox-wrapper > div > div.mint-msgbox-btns > button.mint-msgbox-btn.mint-msgbox-confirm.mt-btn-primary"
+
+
+def get_decode_image(page):
+    page.locator("#img1").wait_for(timeout=10000, state="attached")
+    src1 = page.locator("#img1").get_attribute("src")
+    im_base64 = src1.split(',')[1]  # 拿到base64编码的图片信息
+    im_bytes = base64.b64decode(im_base64)  # 转为bytes类型
+    with open('bg.png', 'wb') as f:  # 保存图片到本地
+        f.write(im_bytes)
+
+    page.locator("#img2").wait_for(timeout=1000, state="attached")
+    src2 = page.locator("#img2").get_attribute("src")
+    im_base64 = src2.split(',')[1]  # 拿到base64编码的图片信息
+    im_bytes = base64.b64decode(im_base64)  # 转为bytes类型
+    with open('front.png', 'wb') as f:  # 保存图片到本地
+        f.write(im_bytes)
+
+
+def log_in(page):
+    url = 'https://eportal.uestc.edu.cn/jkdkapp/sys/lwReportEpidemicStu/*default/index.do'
+
+    page.goto(url)
+    page.fill("#mobileUsername", username)
+    page.fill("#mobilePassword", password)
+    page.click("#load")
+
+    for i in range(5):
+        try:
+            get_decode_image(page)
+            image1 = "./front.png"
+            # 背景图片
+            image2 = "./bg.png"
+            # 处理结果图片,用红线标注
+            image3 = "./3.png"
+            sc = SlideCrack(image1, image2, image3)
+            distance = int((sc.discern()) * 0.48)  # 280/590
+            print(distance)
+            # 滑动验证：获取要位置的距离
+
+            page.locator('#captcha > div > div.sliderMask > div').wait_for(timeout=10000)
+            elem = page.locator('#captcha > div > div.sliderMask > div')
+
+            elem.hover()
+            page.mouse.down()
+            pos = elem.bounding_box()
+            page.mouse.move(pos['x']+pos['width']/2+distance, pos['y'] + 15, steps=35)
+            page.mouse.up()
+            print("Loading...")
+            page.wait_for_timeout(2000)
+
+        except Exception as e:
+            print("Error loading or swiping captcha image. ", e)
+            continue
+
+        try:
+            page.locator("#mobileUsername").wait_for(timeout=1000)
+            print("Verification failed")
+            continue
+        except:
+            print("Verified")
+            return
+    page.locator(check_button).wait_for(timeout=10000)
+
+def check():
+    with sync_playwright() as p:
+        browser = p.webkit.launch(headless=False)
+        context = browser.new_context(user_agent='Mozilla/5.0 (iPhone; CPU iPhone OS 13_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148')
+
+        page = context.new_page()
+        for i in range(3):
+            try:
+                log_in(page)
+                print("Login successful")
+                break
+            except Exception as e:
+                print("Login failed for ", e)
+                continue
+        logtime = time()
+        try:
+            page.locator(check_button).wait_for(timeout=30000)
+        except:
+            print("Reload page")
+            page.reload()
+        print("Check login status:", time() - logtime)
+        page.locator(check_button).wait_for(timeout=30000)
+        print("Check login status:", time() - logtime)
+
+        page.wait_for_timeout(5000)
+
+        try:
+            page.locator(add_button).wait_for(timeout=10000)
+            page.click(add_button)
+            print("Add OK ", time() - logtime)
+            page.wait_for_timeout(1000)
+            page.keyboard.press('ArrowDown', delay=500)
+            page.wait_for_timeout(1000)
+
+            page.locator(save_button).wait_for(timeout=10000)
+            page.click(save_button)
+            print("Save OK ", time() - logtime)
+        except:
+            print("Repeat report")
+            browser.close()
+            return
+
+        page.wait_for_timeout(1000)
+        page.locator(confirm_button).wait_for(timeout=10000)
+        page.click(confirm_button)
+        print("Confirm OK ", time() - logtime)
+        page.wait_for_timeout(1000)
+        browser.close()
+        print("Successfully reported")
+
+
+if __name__ == "__main__":
+    check()
